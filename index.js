7f66b29c422f239acd1178b124b6454225764334
James Thomas <jamesjacobthomas7@gmail.com>
2018-04-06 15:32:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 
-function takeANumber(katzDeliLine, name) {
-    katzDeliLine.push(name)
-    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
-  }
+  let number = 1
+function takeANumber(line) {
+  line.push(number)
+  number++
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
 
-
-function nowServing(deliLine) {
-  if (deliLine.length === 0) {
-    return 'There is nobody waiting to be served!'
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
   } else {
-  var firstPerson =  deliLine.shift()
-  return  `Currently serving ${firstPerson}.`
+    return `Currently serving ${line.shift()}.`
   }
 }
-//["Ada", "Grace"]
 
-function currentLine(line) {
+
+
+ function currentLine(line) {
   if (line.length === 0) {
     return 'The line is currently empty.'
   } else {
-
-    var status = 'The line is currently: '
-    for ( var position = 1; position < line.length + 1; position++) {
-
-      if (position === line.length) {
-        status = status + `${position}. ${line[position - 1]}`
-      } else {
-        status = status + `${position}. ${line[position - 1]}, `
-      }
+    let string = 'The line is currently:'
+    for (var i = 0; i < line.length; i++) {
+      string = `${string} ${i+1}. ${line[i]},`
+    }
+    return string.slice(0, -1)
   }
-  return status
-}
 }
